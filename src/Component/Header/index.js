83c8ddacee45489f89abffa6ce2f7c8f88df9cfb
krisha bhingaradiya <krishabhingaradiya1234@gmail.com>
2024-04-23 13:32:35 +0530

    etup Mobile layout

--- src/Component/Header/index.js
+++ src/Component/Header/index.js
@@ -1,34 +1,126 @@
-import { Box, Flex, Grid, GridItem, Icon, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  GridItem,
+  Icon,
+  Tab,
+  Tabs,
+  Text,
+} from '@chakra-ui/react';
 import React from 'react';
 import { CiLink } from 'react-icons/ci';
-import { NavLink, Outlet, useLocation, useParams } from 'react-router-dom';
+import { FaLink } from 'react-icons/fa6';
+import { CgProfile } from 'react-icons/cg';
+import { AiOutlineEye } from 'react-icons/ai';
+
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
 import Mobile from '../Mobile';
 
 const Header = () => {
   const location = useLocation();
-  console.log(location.pathname);
   const path = location.pathname.includes('preview');
-  console.log(path);
+
   return (
     <>
-      <Flex flexDir='column' h='100vh' w='100vw'>
+      <Flex flexDir='column' h='100vh' width='100vw' bg='#dff7ff' p='24px 44px'>
         {path ? (
-          <Flex alignItems='center' justifyContent='space-between'>
-            <NavLink to='/'>Back to Editor</NavLink>
-            <NavLink to='/'>Share Link Editor</NavLink>
+          <Flex
+            alignItems='center'
+            justifyContent='space-between'
+            bg='white'
+            p='8px'
+          >
+            <NavLink to='/'>
+              <Button border='1px solid #7750de' color='#7750de'>
+                Back to Editor
+              </Button>
+            </NavLink>
+            <NavLink to='/'>
+              <Button
+                border='1px solid #7750de'
+                color='white'
+                bg='#7750de'
+                _hover={{ color: 'white' }}
+              >
+                Share Link
+              </Button>
+            </NavLink>
           </Flex>
         ) : (
-          <Flex alignItems='center' justifyContent='space-between'>
-            <Flex alignItems='center'>
-              <Icon as={CiLink} />
-              <Text>devlinks</Text>
+          <Flex
+            alignItems='center'
+            justifyContent='space-between'
+            bg='white'
+            p='8px'
+            fontSize='20px'
+          >
+            <Flex alignItems='center' gap='8px'>
+              <Icon as={CiLink} bg='#6511f6' color='white' borderRadius='4px' />
+              <Text
+                lineHeight='normal'
+                fontWeight='700'
+                display={{ base: 'none', sm: 'block' }}
+              >
+                devlinks
+              </Text>
             </Flex>
+
+            <Tabs>
+              <Flex gap='8px' color='#737373'>
+                <NavLink to='/'>
+                  <Tab
+                    _selected={{
+                      bg: '#efe9fe',
+                      color: '#7750de',
+                      borderRadius: '6px',
+                    }}
+                  >
+                    <Flex alignItems='center' gap='8px' p='4px'>
+                      <Icon as={FaLink} />
+                      <Text
+                        lineHeight='normal'
+                        display={{ base: 'none', sm: 'block' }}
+                      >
+                        Links
+                      </Text>
+                    </Flex>
+                  </Tab>
+                </NavLink>
+
+                <NavLink to='/profile'>
+                  <Tab
+                    _selected={{
+                      bg: '#efe9fe',
+                      color: '#7750de',
+                      borderRadius: '6px',
+                    }}
+                  >
+                    <Flex alignItems='center' gap='8px' p='4px'>
+                      <Icon as={CgProfile} />
+                      <Text
+                        lineHeight='normal'
+                        display={{ base: 'none', sm: 'block' }}
+                      >
+                        Profile Details
+                      </Text>
+                    </Flex>
+                  </Tab>
+                </NavLink>
+              </Flex>
+            </Tabs>
+
             <Box>
-              <NavLink to='/'>Links</NavLink>
-              <NavLink to='/profile'>ProfileDetails</NavLink>
-            </Box>
-            <Box>
-              <NavLink to='/preview'>Preview</NavLink>
+              <NavLink to='/preview'>
+                <Button border='1px solid #7750de' color='#7750de'>
+                  <Icon
+                    as={AiOutlineEye}
+                    display={{ base: 'block', sm: 'none' }}
+                  />
+                  <Text display={{ base: 'none', sm: 'block' }}>Preview</Text>
+                </Button>
+              </NavLink>
             </Box>
           </Flex>
         )}
--- src/Component/Mobile/index.js
+++ src/Component/Mobile/index.js
@@ -1,8 +1,13 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Image } from '@chakra-ui/react';
 import React from 'react';
+import mobileDesign from '../../assets/mobileDesign.png';
 
 const Mobile = () => {
-  return <Box>Mobile</Box>;
+  return (
+    <Flex h='calc(100vh - 82px)' alignItems='center' justifyContent='center'>
+      <Image src={mobileDesign} h='600px' />
+    </Flex>
+  );
 };
 
 export default Mobile;
